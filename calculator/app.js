2febc3ca9951ff6388aa7573716440c0d1dfa4c1
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-08-08 23:43:15 +0530

    functionality complete, need a little bit of tweaking and styleing, also try to use component feature of EJS here for result

---
+++ calculator/app.js
@@ -0,0 +1,53 @@
+const bodyParser=require('body-parser');
+const express=require('express');
+const ejs=require('ejs');
+
+let app=express();
+let port=process.env.PORT||8000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine','ejs');
+
+let result='null';
+let num1;
+let num2;
+let operator;
+
+app.get('/',function(req,res){
+	if(result!='null'){
+	// res.write(`<h1>the result is :- ${result}</h1>`)
+	// res.write(`<h1>the first no. is :- ${num1}</h1>`)
+	// res.send();
+	res.render('index',{x:num1, y:num2, op:operator, result:result})
+}
+	res.sendFile(__dirname+'/index.html');
+});
+
+app.post('/',function(req,res){
+	 num1=parseInt(req.body.operand1);
+	 num2=parseInt(req.body.operand2);
+	 operator=req.body.operator;
+	switch(operator){
+		case "/":
+		result=num1/num2;
+		break;
+		case "+":
+		result=num1+num2;
+		break;
+		case "-":
+		result=num1-num2;
+		break;
+		case "*":
+		result=num1*num2;
+		break;
+		case "%":
+		result=num1%num2;
+		break;
+	}
+	res.redirect('/');
+});
+
+app.listen(port,function(){
+	console.log(`server online at port no. ${port}`);
+});
